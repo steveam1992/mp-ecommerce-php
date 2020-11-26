6ca24a392a1bff68c9a64b7a8eca90d612b85dc5
steveam1992 <samdub92@gmail.com>
2020-11-26 11:18:22 -0700

    ya con datos extraidos

--- detail.php
+++ detail.php
@@ -1,8 +1,6 @@
-
-
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
+
     <meta name="viewport" content="width=1024">
     <title>Tienda e-commerce</title>
 
@@ -10,9 +8,9 @@
     <meta name="format-detection" content="telephone=no">
 
     <script
-    src="https://code.jquery.com/jquery-3.4.1.min.js"
-    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-    crossorigin="anonymous"></script>
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
 
@@ -20,11 +18,7 @@
 
     <link rel="stylesheet" href="./assets/merch-tools.css" media="screen, print">
 
-    <link rel="stylesheet" href="./wss" media="">
-    <script src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"></script>
-   <script>
-   window.Mercadopago.setPublishableKey("TEST-151a7d2e-e1b3-4a16-bb5f-97519ec58d17");
-   </script>
+    <link rel="stylesheet" href="./assets/fonts" media="">
     <style>
         .as-filter-button-text {
             font-size: 26px;
@@ -49,124 +43,108 @@
 
 <body class="as-theme-light-heroimage">
 
-    <div class="stack">
-        
-        <div class="as-search-wrapper" role="main">
-            <div class="as-navtuck-wrapper">
-                <div class="as-l-fullwidth  as-navtuck" data-events="event52">
-                    <div>
-                        <div class="pd-billboard pd-category-header">
-                            <div class="pd-l-plate-scale">
-                                <div class="pd-billboard-background">
-                                    <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
-                                </div>
-                                <div class="pd-billboard-info">
-                                    <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
-                                </div>
+<div class="stack">
+
+    <div class="as-search-wrapper" role="main">
+        <div class="as-navtuck-wrapper">
+            <div class="as-l-fullwidth  as-navtuck" data-events="event52">
+                <div>
+                    <div class="pd-billboard pd-category-header">
+                        <div class="pd-l-plate-scale">
+                            <div class="pd-billboard-background">
+                                <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
+                            </div>
+                            <div class="pd-billboard-info">
+                                <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
+        </div>
+        <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
 
-                <div id="accessories-tab" class="as-accessories-details">
-                    <div class="as-accessories" id="as-accessories">
-                        <div class="as-accessories-header">
-                            <div class="as-search-results-count">
-                                <span class="as-search-results-value"></span>
+            <div id="accessories-tab" class="as-accessories-details">
+                <div class="as-accessories" id="as-accessories">
+                    <div class="as-accessories-header">
+                        <div class="as-search-results-count">
+                            <span class="as-search-results-value"></span>
+                        </div>
+                    </div>
+                    <div class="as-searchnav-placeholder" style="height: 77px;">
+                        <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
+                            <div class="as-accessories-filter-tile column large-6 small-3">
+                                <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
+                                    <h2 class=" as-filter-button-text">
+                                        Smartphones
+                                    </h2>
+                                </button>
                             </div>
                         </div>
-                        <div class="as-searchnav-placeholder" style="height: 77px;">
-                            <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
-                                <div class="as-accessories-filter-tile column large-6 small-3">
+                    </div>
+                    <div class="as-accessories-results  as-search-desktop">
+                        <div class="width:60%">
+                            <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
+                                <div class="as-dummy-container as-dummy-img">
 
-                                    <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
-                                        <h2 class=" as-filter-button-text">
-                                            Smartphones
-                                        </h2>
-                                    </button>
-
-
+                                    <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
                                 </div>
+                                <div class="images mini-gallery gal5 ">
 
-                            </div>
-                        </div>
-                        <div class="as-accessories-results  as-search-desktop">
-                            <div class="width:60%">
-                                <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
-                                    <div class="as-dummy-container as-dummy-img">
 
-                                        <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
-                                    </div>
-                                    <div class="images mini-gallery gal5 ">
-                                    
-
-                                        <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
-                                            <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
-                                                <div class="as-tilegallery-element as-image-selected">
-                                                    <div class=""></div>
-                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
-                                                </div>
-                                                
+                                    <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
+                                        <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
+                                            <div class="as-tilegallery-element as-image-selected">
+                                                <div class=""></div>
+                                                <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
                                             </div>
 
-                                            
                                         </div>
 
-                                        
 
                                     </div>
 
-                                </div>
-                                <div class="as-producttile-info" style="float:left;min-height: 168px;">
 
-                                    <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
-                                <form action="process.php" method="POST">
-                                <input type="hidden" name="title" value="<?php echo $_POST['title']; ?>">
+
+                                </div>
+
+                            </div>
+                            <div class="as-producttile-info" style="float:left;min-height: 168px;">
+                                <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
+                                    <form action="process.php" method="POST">
+                                        <input type="hidden" name="title" value="<?php echo $_POST['title']; ?>">
                                         <input type="hidden" name="price" value="<?php echo $_POST['price']; ?>">
                                         <input type="hidden" name="unit" value=" <?php echo $_POST['unit']; ?>">
                                         <div class="as-producttile-title">
                                             <h3 class="as-producttile-name">
                                                 <p class="as-producttile-tilelink">
-                                                    <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
+                                                    <span data-ase-truncate="2"><?php echo $_POST['title']; ?></span>
                                                 </p>
 
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo "$" .$_POST['price'] ?>
+                                            <?php echo $_POST['unit'] ?>
                                         </h3>
                                         <h3 >
-                                            <?php echo  $_POST['unit'] ?>
+                                            <?php echo "$" . $_POST['price'] ?>
                                         </h3>
-                                    </div>
-                                  
-                                    <script
-                                    src="https://www.mercadopago.com.mx/integrations/v1/web-tokenize-checkout.js"
-                                    data-public-key="TEST-151a7d2e-e1b3-4a16-bb5f-97519ec58d17"
-                                    data-transaction-amount="<?php echo $_POST['price']; ?>" data-button-label="Pagar con mercadoPago">
-                                    </script>
-                                    
-                                </form>
-                                </div>
+                                        <script src="https://www.mercadopago.com.mx/integrations/v1/web-tokenize-checkout.js"data-public-key="TEST-151a7d2e-e1b3-4a16-bb5f-97519ec58d17"data-transaction-amount="<?php echo $_POST['price']; ?>" data-button-label="Pagar con mercadoPago"></script>
+                                    </form>
 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div role="alert" class="as-loader-text ally" aria-live="assertive"></div>
-        <div class="as-footnotes">
-            <div class="as-footnotes-content">
-                <div class="as-footnotes-sosumi">
-                    Todos los derechos reservados Tienda Tecno 2019
-                </div>
+    </div>
+    <div role="alert" class="as-loader-text ally" aria-live="assertive"></div>
+    <div class="as-footnotes">
+        <div class="as-footnotes-content">
+            <div class="as-footnotes-sosumi">
+                Todos los derechos reservados Tienda Tecno 2019
             </div>
         </div>
-
-</div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div class="mp-mercadopago-checkout-wrapper" style="z-index:-2147483647;display:block;background:rgba(0, 0, 0, 0.7);border:0;overflow:hidden;visibility:hidden;margin:0;padding:0;position:fixed;left:0;top:0;width:0;opacity:0;height:0;transition:opacity 220ms ease-in;"> <svg class="mp-spinner" viewBox="25 25 50 50"> <circle class="mp-spinner-path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"></circle> </svg> </div><div id="ac-gn-viewport-emitter"> </div>
-
-
-</body></html>
+    </div>
--- process.php
+++ process.php
@@ -4,22 +4,10 @@ require_once 'vendor/autoload.php'; // You have to require the library from your
 MercadoPago\SDK::setAccessToken("TEST-5986425512030588-112517-3bece8b99c747f9cf1af03c5295db77c-169100115"); // Either Production or SandBox AccessToken
 
 $token = $_REQUEST["token"];
-$id = $_REQUEST["id"];
 $method_id = $_REQUEST["payment_method_id"];
 $installments = $_REQUEST["installments"];
 $issuer_id = $_REQUEST["issuer_id"];
 $total = floatval($_POST['unit'] * $_POST['price']);
-
-
-$preference = new MercadoPago\Preference();
-
-$item = new MercadoPago\Item();
-$item->id = $id;
-$item->description = $_POST['title'];
-$item->title = htmlspecialchars( $_POST["title"] );
-$item->quantity = intval( $_POST["unit"] );
-$item->unit_price = floatval( $_POST["price"] );
-$preference->items = array( $item );
 
 
 
@@ -30,7 +18,7 @@ $payment->description = $_POST['title'];
 $payment->installments = $installments;
 $payment->payment_method_id = $method_id;
 $payment->issuer_id = $issuer_id;
-$payment->id = $id;
+
 
 $payment->payer = array(
     "email" => "example@gmail.com"
@@ -38,7 +26,9 @@ $payment->payer = array(
 $payment->save();
 
 
-var_dump($payment);
+
+//  var_dump($payment);
+echo json_encode(array('id'=>$payment->id,'estatus'=>$payment->status,'status_detail'=>$payment->status_detail,'description'=>$payment->description, 'live_mode'=>$payment->live_mode, 'collector_id'=>$payment->collector_id,'payment_method_id'=>$payment->payment_method_id ));
 
 
 ?>
